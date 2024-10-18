94bdd7c08acd253280fe0768a153552605b20027
Adam Troyan <58230709+AdamTroyan@users.noreply.github.com>
2024-10-18 04:10:00 +0300

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -17,7 +17,7 @@ public class UserInterface {
 
     public void start() {
         boolean continueRunning = true;
-        System.out.flush();
+        clearScreen();
         System.out.println(GREEN + "Welcome to Encryptora!" + RESET);
 
         while (continueRunning) {
@@ -41,6 +41,19 @@ public class UserInterface {
             }
         }
         scanner.close();
+    }
+
+     public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void displayMenu() {
